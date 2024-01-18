4bf5f56d0b837e61259211cf8838f792f1248bc7
Dzoibo <ivandztitan@gmail.com>
2024-01-18 18:04:52 +0100

    feat: animate button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { gsap } from 'gsap';
@@ -10,11 +10,40 @@ import { gsap } from 'gsap';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'airpods-max';
   gsapDuration=0.75;
+  focusedHeadset=2;
+  headsetList=[
+    {
+      id:1,
+      name:'argent',
+    },
+    {
+      id:2,
+      name: 'noir',
+    },
+    {
+      id:3,
+      name: 'pink'
+    }
+  ]
 
-  rotateOnClick(){
+ngOnInit(): void {
+}
+
+
+  showNext(){
+    if(this.focusedHeadset===3){
+      this.showPrev()
+      return;
+    }
+    this.focusedHeadset=this.focusedHeadset+1;
+    setTimeout(() => {
+      this.showHideButton("next");
+      this.fill_background();
+    }, 50);
+    console.log(this.focusedHeadset);
     gsap.to(".headsetA", {
        rotation: 0,
        duration: this.gsapDuration,
@@ -46,10 +75,111 @@ export class AppComponent {
 
   }
 
+  showPrev(){
+    if(this.focusedHeadset===1){
+      return;
+    }
+    this.focusedHeadset=this.focusedHeadset-1;
+      setTimeout(() => {
+      this.showHideButton("prev");
+      this.fill_background();
+    }, 50);
+  }
+
   fill_background(){
-    gsap.to(".animated-bacground",{
-      height: 120,
-      
+    gsap.to(".blue-slider",{
+      duration: this.gsapDuration,
+      x: (this.focusedHeadset-1) * (20+94)// 20 is the gap between items and 94 is the width of the item
+    });
+
+    gsap.to(".show-animated-background",{
+      height: 179,
+      duration: this.gsapDuration
+    });
+
+    gsap.to(".remove-animated-background",{
+      height: 7,
+      duration: this.gsapDuration
     })
+  }
+
+  showHideButton(direction: string){
+    if(this.focusedHeadset===1){
+      gsap.to(".next",{
+        x:-35,
+        duration: this.gsapDuration
+      });
+      gsap.to(".prev",{
+        x:35,
+        duration: this.gsapDuration
+      });
+    }else if(this.focusedHeadset===2 && direction==='next'){
+      gsap.to(".next",{
+        x:0,
+        duration: this.gsapDuration
+      });
+      gsap.to(".prev",{
+        x:0,
+        duration: this.gsapDuration
+      });
+      gsap.to((".next-text"),{
+        opacity: 0,
+        position: "absolute",
+        duration: this.gsapDuration
+      });
+      gsap.to((".prev-text"),{
+        opacity: 1,
+        position: "relative",
+        duration: this.gsapDuration
+      })
+    }else if(this.focusedHeadset===2 && direction!=='next'){
+      gsap.to(".next",{
+        x:0,
+        duration: this.gsapDuration
+      });
+      gsap.to(".prev",{
+        x:0,
+        duration: this.gsapDuration
+      });
+      gsap.to((".prev-text"),{
+        opacity: 1,
+        position: "relative",
+        duration: this.gsapDuration
+      });
+      gsap.to((".next-icon"),{
+        rotate: 0,
+        marginLeft:0,
+        duration: this.gsapDuration
+      });
+      gsap.to((".next-text"),{
+        opacity: 0,
+        position: "absolute",
+        duration: this.gsapDuration
+      })
+    }else{
+      gsap.to(".next",{
+        x:-35,
+        duration: this.gsapDuration
+      });
+      gsap.to(".prev",{
+        x:35,
+        duration: this.gsapDuration
+      });
+      gsap.to((".next-text"),{
+        opacity: 1,
+        position: "relative",
+        duration: this.gsapDuration
+      });
+      gsap.to((".prev-text"),{
+        opacity: 0,
+        position: "absolute",
+        duration: this.gsapDuration
+      });
+      gsap.to((".next-icon"),{
+        rotate: 180,
+        marginLeft: 40,
+        duration: this.gsapDuration
+      });
+    }
   }
 }
