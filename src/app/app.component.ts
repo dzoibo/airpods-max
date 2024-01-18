cf8c0517caed3fb55b63fd196cfd2546a55a7400
Dzoibo <ivandztitan@gmail.com>
2024-01-18 15:26:09 +0100

    adding gsap library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,44 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'airpods-max';
+  gsapDuration=0.75;
+
+  rotateOnClick(){
+    gsap.to(".headsetA", {
+       rotation: 0,
+       duration: this.gsapDuration,
+       left: "50%",
+       x:"-50%",
+       height:630,
+       width:498,
+       });
+    gsap.to(".headsetC",{
+      height: 150,
+      duration: this.gsapDuration,
+      width: 119,
+      left: -20,
+      bottom:0,
+      rotation:121.208,
+      x:0,
+    })
+    gsap.to(".headsetB",{
+      duration: 0.5,
+      rotation:360,
+      x:-200,
+      translateY:200,
+    });
+    gsap.to(".headsetB",{
+      duration: 0.2,
+      opacity:0,
+      delay:0.3
+    })
+
+  }
+
+  fill_background(){
+    gsap.to(".animated-bacground",{
+      height: 120,
+      
+    })
+  }
 }
